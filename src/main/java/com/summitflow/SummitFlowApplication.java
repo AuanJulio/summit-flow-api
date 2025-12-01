78796158d3d78a85dcc5050abdc92d6dea7b4801
AuanJulio <auanjulio13@gmail.com>
2025-12-01 02:06:32 -0300

    first commit

---
+++ src/main/java/com/summitflow/SummitFlowApplication.java
@@ -0,0 +1,13 @@
+package com.summitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummitFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummitFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/summitflow/SummitFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummitFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
