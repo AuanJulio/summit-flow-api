7d39aa519de7ca30f547e76071dbf00f26ff96b9
AuanJulio <auanjulio13@gmail.com>
2025-12-18 03:01:17 -0300

    feat: implement CRUD endpoints in TrackController and mock logic in TrackService

--- src/main/java/com/summitflow/controller/TrackController.java
+++ src/main/java/com/summitflow/controller/TrackController.java
@@ -1,9 +1,11 @@
 package com.summitflow.controller;
 
+import com.summitflow.entity.Track;
 import com.summitflow.service.TrackService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/tracks")
@@ -11,5 +13,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class TrackController {
 
     private final TrackService trackService;
+
+    @GetMapping
+    public List<Track> getAll(){
+        return trackService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Track getById(@PathVariable Long id){
+        return trackService.findById(id);
+    }
+
+    @PostMapping
+    public Track save(@RequestBody Track track){
+        return trackService.save(track);
+    }
+
+    @PutMapping("/{id}")
+    public Track update(@PathVariable Long id, @RequestBody Track track){
+        return trackService.update(id, track);
+    }
+
+    @DeleteMapping
+    public void delete(@PathVariable Long id){
+        trackService.deleteById(id);
+    }
 
 }
--- src/main/java/com/summitflow/service/TrackService.java
+++ src/main/java/com/summitflow/service/TrackService.java
@@ -1,13 +1,37 @@
 package com.summitflow.service;
 
+import com.summitflow.entity.Track;
 import com.summitflow.repository.TrackRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TrackService {
 
     private final TrackRepository trackRepository;
+
+    public List<Track> findAll(){
+        return trackRepository.findAll();
+    }
+
+    public Track findById(Long id){
+        return trackRepository.findById(id).get();
+    }
+
+    public Track save(Track track){
+        return trackRepository.save(track);
+    }
+
+    public Track update(Long id, Track track){
+        track.setId(id);
+        return trackRepository.save(track);
+    }
+
+    public void deleteById(Long id){
+        trackRepository.deleteById(id);
+    }
 
 }
