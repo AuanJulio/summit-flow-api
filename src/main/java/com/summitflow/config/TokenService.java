49e62a3fa5246b1200bebe627e4dcb27f1647d3b
AuanJulio <auanjulio13@gmail.com>
2025-12-24 18:58:33 -0300

    feat: create TokenService to generate/verify JWTs and configure environment-based secret injection

---
+++ src/main/java/com/summitflow/config/TokenService.java
@@ -0,0 +1,51 @@
+package com.summitflow.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.summitflow.controller.response.UserResponse;
+import com.summitflow.entity.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.Optional;
+
+@Component
+public class TokenService {
+
+    @Value( "${summit-flow.jwt.secret}")
+    private String secret;
+
+    public String generateToken(User user){
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        return JWT.create()
+                .withSubject(user.getEmail())
+                .withClaim("userId", user.getId())
+                .withClaim("name", user.getName())
+                .withExpiresAt(Instant.now().plusSeconds(86400))
+                .withIssuedAt(Instant.now())
+                .withIssuer("SummitFlow")
+                .sign(algorithm);
+    }
+
+    public Optional<UserResponse> verifyToken(String token){
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            DecodedJWT decodedJWT = JWT.require(algorithm).build().verify(token);
+
+            return Optional.of(UserResponse.builder()
+                    .id(decodedJWT.getClaim("userId").asLong())
+                    .name(decodedJWT.getClaim("name").asString())
+                    .email(decodedJWT.getSubject())
+                    .build());
+        } catch (JWTVerificationException e) {
+            return Optional.empty();
+        }
+    }
+
+}
