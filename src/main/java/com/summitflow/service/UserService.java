1172196ef95244631fba8973d64ff257da4470d3
AuanJulio <auanjulio13@gmail.com>
2025-12-24 18:19:55 -0300

    refactor: integrate PasswordEncoder to encrypt passwords in UserService save method

--- src/main/java/com/summitflow/service/UserService.java
+++ src/main/java/com/summitflow/service/UserService.java
@@ -3,6 +3,7 @@ package com.summitflow.service;
 import com.summitflow.entity.User;
 import com.summitflow.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +11,11 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User save(User user){
+        String passord = user.getPassword();
+        user.setPassword(passwordEncoder.encode(passord));
         return userRepository.save(user);
     }
 
